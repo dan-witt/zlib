99cb94d1483ee01a18cf374764f4164784182993
Daniel Witt <dan.witt@gmail.com>
2023-08-15 08:07:02 -0500

    add presets and simple script to build and bundle for android

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#if ANDROID_NDK not set look in ANDROID_NDK search paths and set it to the highest version
+
+if [ "$ANDROID_NDK" == "" ]; then
+    ANDROID_NDK_SEARCH_PATHS=("$HOME/Android/Sdk/ndk" "/srv")
+    
+    for search_path in "${ANDROID_NDK_SEARCH_PATHS[@]}"; do
+        if [ -d "$search_path" ]; then
+            highest_version=$(ls "$search_path" | sort | tail -1)
+            ANDROID_NDK="$search_path/$highest_version"
+            echo "Found NDK at $ANDROID_NDK"
+            export ANDROID_NDK 
+            break
+        fi
+    done
+fi
+
+archs=('android-arm' 'android-arm64' 'android-x86_64' 'android-x86' 'linux')
+
+for arch in "${archs[@]}"; do
+    cmake --preset $arch
+    cmake --build --preset $arch
+done
